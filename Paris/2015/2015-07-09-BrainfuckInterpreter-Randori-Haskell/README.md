df4dc27ba05377927fd56d59b5faab6aac011605
Philippe Bourgau <philippe.bourgau@gmail.com>
2015-08-18 09:40:12 +0200

    2015-07-09 session, Brainfuck interpreter Randori Haskell

---
+++ Paris/2015/2015-07-09-BrainfuckInterpreter-Randori-Haskell/README.md
@@ -0,0 +1,27 @@
+# Subject
+
+Subject | Votes
+-|-
+Mastermind | *****
+brainfuck |	**********
+poker hand | *********
+
+# Language
+
+Language | Votes
+-|-
+Haskell	| ******************
+Ruby |	******
+Python |	***
+Java |	*********
+
+# Retrospective
+
+* Pas facile à comprendre au début, mais marrant
+* Haskell nous a bien aidé avec beaucoup de matching
+* On était bien parti pour réussir à faire encore plus simple et passer le test
+* Bien mais on s'est arrêté avant le plus dur ([ imbriqués, retours en arrière ...)
+
+# The code
+
+On cyber dojo http://cyber-dojo.org/dashboard/show/DB703C1500
---
+++ Paris/2015/2015-07-09-BrainfuckInterpreter-Randori-Haskell/hiker.hs
@@ -0,0 +1,26 @@
+module Hiker where
+import Data.Char
+data Memory = Memory{left::[Int], ptr::Int, right::[Int]}
+    deriving(Read,Show,Eq)
+ 
+blankMemory = (Memory (repeat 0) 0 (repeat 0))
+ 
+ 
+inc (Memory left x xs) = Memory left (x+1) xs
+dec (Memory left x xs) = Memory left (x-1) xs
+fwd (Memory left x (y:xs)) = Memory (x:left) y xs
+bwd (Memory (x:xs) y right) = Memory xs x (y:right)
+ 
+ 
+eval :: String -> String
+eval = eval' blankMemory
+ 
+ 
+eval' :: Memory -> String -> String
+eval' _ "" = ""
+eval' mem ('+':expr) = eval' (inc mem) expr
+eval' mem ('-':expr) = eval' (dec mem) expr
+eval' mem ('.':expr) = (chr(ptr mem):eval' mem expr)
+eval' mem ('>':expr) = eval' (fwd mem) expr
+eval' mem ('<':expr) = eval' (bwd mem) expr
+eval' mem@(Memory _ 0 _) ('[':_:']':expr) = eval' mem expr
