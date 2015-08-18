a3efbe2aca3837889dcb0279deec6b0de9b61498
Philippe Bourgau <philippe.bourgau@gmail.com>
2015-08-18 09:36:53 +0200

    Save session 2015-07-17 Brainfuck Interpreter in Haskell

---
+++ Paris/2015/2015-07-17-BrainfuckInterpreter-Continued-Randori-Haskell/README.md
@@ -0,0 +1,9 @@
+# Retrospective
+
+* Que des stagiaire aujourd'hui ? Mais ou sont donc les membres du pop ? (a cause de la nourriture payante ???)
+* Difficulté sur la syntaxe de Haskell
+* Pas réussi à trouver une solution élégante au problème
+
+# The code
+
+On cyber dojo http://cyber-dojo.org/dashboard/show/DB703C1500
---
+++ Paris/2015/2015-07-17-BrainfuckInterpreter-Continued-Randori-Haskell/test_hiker.hs
@@ -0,0 +1,22 @@
+module Test_Hiker where
+ 
+import Test.HUnit
+import Hiker
+ 
+tests = test [
+    --"It should print Hello World" ~: "Hello World" ~=? eval "++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>."
+    "An empty program returns an empty string" ~: "" ~=? eval "",
+    "Print start address" ~: "\0" ~=? eval ".",
+    "Increment and print" ~: "\1" ~=? eval "+.",
+    "Increment twice and print" ~: "\2" ~=? eval "++.",
+    "Increment, decrement and print" ~: "\0" ~=? eval "+-.",
+    "Increment, forward and print" ~: "\0" ~=? eval "+>.",
+    "Double print" ~: "\0\0" ~=? eval "..",
+    "Increment, forward, backward and print" ~: "\1" ~=? eval "+><.",
+    "Bracket can jump out" ~: "\0" ~=? eval "[+].",
+    "Loop while with one iteration should return 0" ~: "\0" ~=? eval "+[-].",
+    "Loop re-enter brackets" ~: "\0" ~=? eval "++[-].",
+    "Several print" ~: "\0\1" ~=? eval ".+.",
+    "move inside bracket" ~: "\0" ~=? eval "[--]."
+   ]
+main = do runTestTT tests
