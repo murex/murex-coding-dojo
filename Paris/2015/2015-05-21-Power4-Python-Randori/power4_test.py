cb9f38a7a78823916de841011efe66a9ea046ad2
Philippe Bourgau <philippe.bourgau@gmail.com>
2015-08-19 09:52:00 +0200

    Fix powe4_test.py

--- Paris/2015/2015-05-21-Power4-Python-Randori/power4_test.py
+++ Paris/2015/2015-05-21-Power4-Python-Randori/power4_test.py
@@ -1,32 +1,57 @@
-from CountCulture import *
- 
-__author__ = 'rjulien'
-import unittest
- 
-UP_TO_10 = [i for i in range(1, 11)]
- 
- 
-class TestCountCulture(unittest.TestCase):
-    def test_acceptance_test(self):
-        self.assertEqual(len(twist_count(91)), 20)
- 
-    def test_count_to_1_should_return_1(self):
-        self.assertEqual(twist_count(1), [1])
- 
-    def test_it_should_incrementally_count_to_10(self):
-        self.assertEqual(twist_count(10), UP_TO_10)
- 
-    def test_count_to_21(self):
-        self.assertEqual(twist_count(21), UP_TO_10 + [11, 12, 21])
- 
-    def test_best_path_return_shortest_path(self):
-        self.assertEqual(best_path([[1], [1, 2]]), [1])
- 
-    def test_best_path_return_shortest_path_not_at_the_beginning(self):
-        self.assertEqual(best_path([[1, 2], [2]]), [2])
- 
-    def test_switch_reverses_the_inputs(self):
-        self.assertEqual(switch(45), 54)
- 
-    def test_switch_drops_the_zeros(self):
-        self.assertEqual(switch(320), 23)
+import power4
+#def test_final():
+#    assert power4.isOver([
+#        [0, 1, 2, 1, 2, 1, 1],
+#        [0, 2, 1, 1, 1, 2, 2],
+#        [0, 1, 2, 2, 2, 1, 1],
+#        [1, 2, 1, 1, 1, 2, 2],
+#        [2, 1, 1, 2, 2, 2, 1],
+#        [1, 2, 2, 1, 2, 1, 2],
+#    ]) == True
+def test_p1_win_one_line():
+    assert power4.isOver([[0, 1, 1, 1, 1, 2, 1]]) == True
+def test_p2_win_one_line():
+    assert power4.isOver([[0, 1, 2, 2, 2, 2, 1]]) == True
+def test_draw_one_line():
+    assert power4.isOver([[0, 1, 2, 1, 2, 2, 1]]) == False
+def test_p1_win_with_two_lines():
+    assert power4.isOver([
+        [0, 1, 2, 1, 2, 2, 1],
+        [0, 1, 1, 1, 1, 2, 1]
+    ]) == True
+def test_p1_wins_one_column():
+    assert power4.isOver([
+        [0],
+        [0],
+        [1],
+        [1],
+        [1],
+        [1]
+    ]) == True
+def test_p2_wins_one_column():
+    assert power4.isOver([
+        [0],
+        [0],
+        [2],
+        [2],
+        [2],
+        [2]
+    ]) == True
+def test_p2_wins_one_column():
+    assert power4.isOver([
+        [0,0],
+        [0,0],
+        [0,1],
+        [2,1],
+        [2,1],
+        [2,1]
+    ]) == True
+def test_diagonal():
+    assert power4.isOver([
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 1],
+        [0, 0, 0, 0, 0, 1, 2],
+        [0, 0, 0, 0, 1, 2, 1],
+        [0, 0, 0, 1, 2, 1, 2],
+    ]) == True
