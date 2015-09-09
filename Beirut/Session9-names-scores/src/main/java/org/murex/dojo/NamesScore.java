04d4556bcc4a14eaec660d06898976494ae649f2
mcmoe <mcxmoe@gmail.com>
2015-09-09 11:10:19 +0200

    initial boiler plate

---
+++ Beirut/Session9-names-scores/src/main/java/org/murex/dojo/NamesScore.java
@@ -0,0 +1,7 @@
+package org.murex.dojo;
+
+/**
+ * Created by mkobeissi on 09/09/2015.
+ */
+public class NamesScore {
+}
---
+++ Beirut/Session9-names-scores/src/test/java/org/murex/dojo/NamesScoreTest.java
@@ -0,0 +1,24 @@
+package org.murex.dojo;
+
+import org.junit.Test;
+
+/**
+ * Unit test the NamesScore algorithm
+ * Created by mkobeissi on 09/09/2015.
+ */
+public class NamesScoreTest {
+
+   @Test
+   public void summing_all_the_names_scores_should_give_me_871198282() {
+      // The sum of all names should be: 871198282
+   }
+
+   @Test
+   public void blabla() {
+      // in the sorted names list, COLIN would obtain a score of 938 × 53 = 49714.
+      // since 938 is his position in the alphabetically sorted list -- that's one possible test for the sorting
+      // COLIN should have a alphabet value of 53 -- that's another possible test for the alphabet values
+      // COLIN's final namescore is another test we can carry out
+
+   }
+}
