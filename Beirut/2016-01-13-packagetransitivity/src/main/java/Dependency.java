2d76fa8f71e40df5e997580e59bb14bd7b8dd3d2
Mrida <mrida88@gmail.com>
2016-01-27 14:44:43 +0200

    [Coding Dojo: Session 2016-01-13]: Package transitivity - Detect cycles

--- Beirut/2016-01-13-packagetransitivity/src/main/java/Dependency.java
+++ Beirut/2016-01-13-packagetransitivity/src/main/java/Dependency.java
@@ -6,25 +6,35 @@ import com.google.common.collect.Sets;
 
 public class Dependency {
 
-   Set<String> result = Sets.newHashSet();
    private ImmutableMap<String, List<String>> dependencies;
+   private Set<String> popped = Sets.newHashSet() ;
 
    public Dependency(ImmutableMap<String, List<String>> dependencies) {
       this.dependencies = dependencies;
    }
 
    public Set<String> compute(String source) {
-      return resolveDependencies(source);
+      Set<String> result = resolveDependencies(source);
+      result.remove(source) ;
+      return result ;
    }
+
 
    private Set<String> resolveDependencies(String source) {
       Set<String> result = Sets.newHashSet();
-      if(dependencies.get(source) != null){
-         for (String dependency : dependencies.get(source)) {
-            result.add(dependency);
-            result.addAll(resolveDependencies(dependency));
+
+      if(!popped.contains(source)) {
+         popped.add(source) ;
+         if (dependencies.get(source) != null) {
+            for (String dependency : dependencies.get(source)) {
+               result.add(dependency);
+               result.addAll(resolveDependencies(dependency));
+            }
          }
+         return result;
       }
       return result;
+
    }
+
 }
