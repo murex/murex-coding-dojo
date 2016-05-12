e1cea3e0b884a0e3f75921ac14204da78ef2c8e4
Mrida <mrida88@gmail.com>
2016-05-12 18:22:18 +0300

    Revert "2016-05-11-java-agent"
    This reverts commit e943c2c1a15b46bdc64329e35080014359599622.

--- Beirut/2016/2016-05-11-java-agent/application/src/main/java/Application.java
+++
@@ -1,9 +0,0 @@
-public class Application {
-   public static void main(String[] args){
-      System.out.println("Main is called");
-   }
-
-   public String hello() {
-      return "hello";
-   }
-}
